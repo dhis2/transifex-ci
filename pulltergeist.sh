3c05c81ab22c0447aaf977647f716cff2ffa88e4
Philip-Larsen-Donnelly <phil@dhis2.org>
2020-04-18 00:48:08 +0200

    feat: added pulltergeist - varl-inspired auto merger

---
+++ pulltergeist.sh
@@ -0,0 +1,135 @@
+#!/usr/bin/env bash
+# Script to exchange translation files between repo and Transifex via Jenkins.
+# It relies on $TXTOKEN and $GHTOKEN being set as env vars for the given job
+
+# set up the python environment
+if [ ! -d "venv" ]; then
+    source setup_venv
+fi
+source ./venv/bin/activate
+
+# Ensure required tools are available
+for exe in "git" "hub" "tx" "jq"; do
+  if [[ ! $(command -v $exe) ]]; then
+    echo "This script requires $exe. Exiting"
+    exit 1
+  fi
+done
+
+# Ensure ENV variables are set
+if [[ -z "$TXTOKEN" ]]; then
+   echo "TXTOKEN environment variable must be set."
+   exit 1
+fi
+if [[ -z "$GITHUB_USER" ]]; then
+   echo "GITHUB_USER environment variable must be set."
+   exit 1
+fi
+if [[ -z "$GITHUB_PASSWORD" ]]; then
+   echo "GITHUB_PASSWORD environment variable must be set."
+   exit 1
+fi
+
+# set -xv
+
+
+GITHUB_BASE="https://github.com/dhis2/"
+GITHUB_CLONE_BASE="https://${GITHUB_USER}:${GITHUB_PASSWORD}@github.com/dhis2/"
+SYNC_DATE=$(date +"%Y%m%d_%H%M%S")
+TX_API=https://www.transifex.com/api/2
+MERGE_FLAG="jenkins-pr-automerge"
+
+# --- options : set the following to `0` to test without pushing anything to remote systems
+PUSH_TRANSLATION_STRINGS=1
+CREATE_PULL_REQUEST=1
+
+
+# --- functions
+git_setup() {
+  git config user.email "apps@dhis2.org"
+  git config user.name "dhis2-bot"
+  git config hub.protocol https
+}
+
+auto_varl () {
+    local BASE="$1"
+    declare -a array
+    readarray -t array < <(hub pr list --base="$BASE" --format='%I|%t%n')
+
+    for PR in "${array[@]}"; do
+        local pr_id="$(echo $PR | cut -d "|" -f 1)"
+        local pr_title="$(echo $PR | cut -d "|" -f 2)"
+        local scope="$(echo $pr_title | cut -d ":" -f 1)"
+        local body="/tmp/${pr_id}-body.json"
+
+        if [ "chore(translations)" == "$scope" ]; then
+            pr_url=$(hub pr show "$pr_id" --url)
+            echo "Transifex PR: ${pr_id} ${pr_title}"
+            echo "${pr_url}"
+            cat << EOF > "$body"
+{
+  "commit_title": "${pr_title}",
+  "commit_message": "Automatically merged.",
+  "merge_method": "squash"
+}
+EOF
+            res=$(hub api --method PUT "repos/{owner}/{repo}/pulls/${pr_id}/merge" --input "$body")
+            echo "Result: $res"
+            rm "$body"
+            sleep 1
+        fi
+    done
+}
+
+# --- starting point
+projects=$(curl -s -L --user api:$TXTOKEN -X GET "$TX_API/projects" | jq '.[].slug')
+mkdir temp$$
+pushd temp$$
+
+for p in $projects; do
+  # Get the name and the git url of the project
+  # The git url is stored in the "homepage" attribute of the transifex project
+  curl -s -L --user api:$TXTOKEN -X GET "$TX_API/project/${p//\"/}?details" >/tmp/proj$$
+  name=$(cat /tmp/proj$$ | jq '.name')
+  tags=$(cat /tmp/proj$$ | jq '.tags')
+  giturl=$(cat /tmp/proj$$ | jq '.homepage')
+  cleanurl=${giturl//\"/}
+  # trim the name of the repo from the full git url (everything after $GITHUB_BASE)
+  gitslug=${cleanurl:${#GITHUB_BASE}}
+  rm /tmp/proj$$
+
+  # Loop through all APP projects
+  if [[ $tags == *"$MERGE_FLAG"* ]]; then
+
+    echo "Attempting to merge translation PRs for $name : $giturl : $gitslug"
+
+    # Get the list of branches translated in this project
+    # The branch names are at the beginnig of each resource slug, followed by double hyphen '--'
+    # The `2.xx` branches appear as `2-xx` and must be converted back (replace hyphen with period)
+    # We only want each branch to be listed once
+    branches=$(curl -s -L --user api:$TXTOKEN -X GET "$TX_API/project/${p//\"/}/resources" | jq '.[].slug | split("--")[0] | split("-") | join(".")' | uniq)
+
+    # clone the project repository and go into it
+    git clone --depth 1 ${GITHUB_CLONE_BASE}${gitslug}
+    pushd "$gitslug"
+    git_setup
+
+    # loop over the branches
+    for b in $branches; do
+      branch=${b//\"/}
+
+      echo "Checking ${branch} branch for translation PRs..."
+      auto_varl $branch
+
+    done
+
+    # leave the repo folder
+    popd
+
+  fi
+
+done
+
+popd
+
+rm -rf temp$$
