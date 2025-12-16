c2a9f771033520878cdfed2607efe0a89d375833
Philip-Larsen-Donnelly <35666657+Philip-Larsen-Donnelly@users.noreply.github.com>
2025-12-16 10:55:15 +0100

    Added debugging features to pulltergeist.sh

--- pulltergeist.sh
+++ pulltergeist.sh
@@ -2,16 +2,22 @@
 # Script to merge translation Pull Reqests in Github via Jenkins.
 # It relies on $TXTOKEN, $GITHUB_USER and $GITHUB_PASSWORD being set as env vars for the given job
 
+# Enable debugging and exit on first error
+set -x
+set -e
 
 # Ensure required tools are available
+echo "Checking required tools..."
 for exe in "git" "hub" "jq" "gh"; do
   if [[ ! $(command -v $exe) ]]; then
     echo "This script requires $exe. Exiting"
     exit 1
   fi
+  echo "$exe is available."
 done
 
 # Ensure ENV variables are set
+echo "Checking environment variables..."
 if [[ -z "$TXTOKEN" ]]; then
    echo "TXTOKEN environment variable must be set."
    exit 1
@@ -24,9 +30,6 @@ if [[ -z "$GITHUB_PASSWORD" ]]; then
    echo "GITHUB_PASSWORD environment variable must be set."
    exit 1
 fi
-
-# set -xv
-
 
 GITHUB_BASE="https://github.com/dhis2/"
 GITHUB_CLONE_BASE="https://${GITHUB_USER}:${GITHUB_PASSWORD}@github.com/dhis2/"
@@ -43,6 +46,7 @@ CLOSE_EXCEPTIONS=("data-visualizer-app" "line-listing-app" "dashboard-app" "maps
 
 # --- functions
 git_setup() {
+  echo "Setting up git configuration..."
   git config user.email "apps@dhis2.org"
   git config user.name "dhis2-bot"
   git config hub.protocol https
@@ -50,6 +54,7 @@ git_setup() {
 
 auto_varl () {
     local BASE="$1"
+    echo "Running auto_varl for base branch: $BASE"
     declare -a array
     readarray -t array < <(hub pr list --base="$BASE" --format='%I|%t|%sH%n')
 
@@ -63,7 +68,7 @@ auto_varl () {
         if [ "fix(translations)" == "$scope" ]; then
             pr_url=$(hub pr show "$pr_id" --url)
             echo "Transifex PR: ${pr_id} ${pr_title}"
-            echo "${pr_url}"
+            echo "PR URL: ${pr_url}"
             cat << EOF > "$body"
 {
   "commit_title": "${pr_title}",
@@ -81,115 +86,46 @@ EOF
             fi
 
             # check the repo name against the exceptions list
-            repo="$(basename `git rev-parse --show-toplevel`)"
-            # if the repo is in the exceptions list, skip closing the PR
-            close_failed_PR=true
-            for value in "${CLOSE_EXCEPTIONS[@]}"; do
-                if [[ ${repo} == "${value}" ]]; then
-                    close_failed_PR=false
-                    break
-                fi
-            done
+            repo="$(basename `git rev-parse --show-toplevel`)">
+            echo "Repository name: $repo">
+            # if the repo is in the exceptions list, skip closing the PR>
+            close_failed_PR=true>
+            for value in "${CLOSE_EXCEPTIONS[@]}"; do>
+                if [[ ${repo} == "${value}" ]]; then>
+                    close_failed_PR=false>
+                    break>
+                fi>
+            done>
             if $close_failed_PR
             then
               # if the CI status is a failure, close the PR (A new one will be opened with changes during the next sync)
               if [[ "${ci_status}" == "failure" ]]
               then
-                  res=$(gh pr close ${pr_id})
-                  echo "Close failed PR. Result: $res"
-              fi
-            fi
-            rm "$body"
+                  res=$(gh pr close ${pr_id})>
+                  echo "Close failed PR. Result: $res">
+              fi>
+            fi>
+            rm "$body">
             sleep 1
-        fi
-    done
+        fi>
+    done>
 }
 
 # --- starting point
-projects=$(curl -s -X GET "$TX_API3/projects?filter[organization]=o:hisp-uio" -H "Content: application/json" -H "Authorization: Bearer $TXTOKEN" | jq '.data[].attributes.slug')
-mkdir temp$$
-pushd temp$$
+echo "Fetching projects from Transifex...">
+projects=$(curl -s -X GET "$TX_API3/projects?filter[organization]=o:hisp-uio" -H "Content: application/json" -H "Authorization: Bearer $TXTOKEN" | jq '.data[].attributes.slug')>
+echo "Projects found: $projects">
+
+mkdir temp$$>
+pushd temp$$>
 
 for p in $projects; do
+  echo "Processing project: $p">
   # Get the name and the git url of the project
-  # The git url is stored in the "homepage" attribute of the transifex project
-  curl -s -X GET "$TX_API3/projects/o:hisp-uio:p:${p//\"/}" -H "Content: application/json" -H "Authorization: Bearer $TXTOKEN" | jq '.[].attributes' >/tmp/proj$$
-  name=$(cat /tmp/proj$$ | jq '.name')
-  tags=$(cat /tmp/proj$$ | jq '.tags | join(",")')
-  giturl=$(cat /tmp/proj$$ | jq '.homepage_url')
-  cleanurl=${giturl//\"/}
-  # trim the name of the repo from the full git url (everything after $GITHUB_BASE)
-  gitslug=${cleanurl:${#GITHUB_BASE}}
-  rm /tmp/proj$$
-
-  # Loop through all APP projects
-  if [[ $tags == *"$MERGE_FLAG"* ]]; then
-
-    echo "Attempting to merge translation PRs for $name : $giturl : $gitslug"
-
-    # Get the list of branches translated in this project
-    # The branch names are at the beginnig of each resource slug, followed by double hyphen '--'
-    # The `2.xx` branches appear as `2-xx` and must be converted back (replace hyphen with period)
-    # We only want each branch to be listed once
-    branches=$(curl -s -X GET "$TX_API3/resources?filter[project]=o:hisp-uio:p:${p//\"/}" -H "Content: application/json" -H "Authorization: Bearer $TXTOKEN" | jq '.data[].attributes.slug | split("--")[0] | split("-") | join(".")' | uniq)
-    # echo "Branches: $branches"
-
-    # clone the project repository and go into it
-    git clone --depth 1 --no-single-branch ${GITHUB_CLONE_BASE}${gitslug}
-    pushd "$gitslug"
-    git_setup
-
-    # loop over the branches
-    for b in $branches; do
-      branch=${b//\"/}
-
-      echo "Checking ${branch} branch for translation PRs..."
-      auto_varl $branch
-
-    done
-    
-    # delete all transifex branches except for unmerged PRs
-    echo "Clean up merged transifex-ALL branches..."
-    hub pr list --format='%H PR#%I%n' > OPEN_PRS
-    for g in $(git branch -r | grep 'transifex-ALL' | sed 's/origin\///')
-    do 
-      if grep -q "$g" OPEN_PRS
-      then
-        # remove any deprecated open PRs
-        deprecated_versions=("v29" "v30" "v31" "v32" "v33" "v34" "v35")
-        deprecated=false
-
-        for value in "${deprecated_versions[@]}"; do
-            if [[ ${g} == *"${value}"* ]]; then
-                deprecated=true
-                break
-            fi
-        done
-
-        if $deprecated
-        then
-            p=$(grep "$g" OPEN_PRS)
-            pr_id=${g/*PR#/}
-            res=$(gh pr close ${pr_id} -c "deprecated version")
-            echo "Close deprecated PR $pr_id. Result: $res"
-            git push -d origin $g
-        else
-            echo "  Skipping open PR: $g"
-        fi
-      else
-        git push -d origin $g
-      fi
-    done
-    rm -f OPEN_PRS
-    echo "Cleaned."
-
-    # leave the repo folder
-    popd
-
-  fi
-
-done
-
-popd
-
-rm -rf temp$$
+  # The git url is stored in the "homepage" attribute of the transifex project>
+  curl -s -X GET "$TX_API3/projects/o:hisp-uio:p:${p//"/}" -H "Content: application/json" -H "Authorization: Bearer $TXTOKEN" | jq '.[].attributes' >/tmp/proj$$>
+  name=$(cat /tmp/proj$$ | jq '.name')>
+  tags=$(cat /tmp/proj$$ | jq '.tags | join(",")')>
+  giturl=$(cat /tmp/proj$$ | jq '.homepage_url')>
+  cleanurl=${giturl//"/">
+  # trim the ...
