38754128e8fd09b6c5c932806a717a09c3321d70
Philip-Larsen-Donnelly <phil@dhis2.org>
2025-12-16 11:48:29 +0100

    debug: pulltergeist

--- pulltergeist.sh
+++ pulltergeist.sh
@@ -25,18 +25,28 @@ if [[ -z "$GITHUB_PASSWORD" ]]; then
    exit 1
 fi
 
-# set -xv
+DEBUG="${DEBUG:-0}"
+
+# safer bash defaults
+set -euo pipefail
+
+# improved xtrace prefix when DEBUG=1
+if [[ "$DEBUG" == "1" ]]; then
+  PS4='+ [${BASH_SOURCE[0]##*/}:${LINENO}] '
+  set -x
+fi
+
+log() {
+  echo "[$(date '+%Y-%m-%dT%H:%M:%S%z')] $*"
+}
+
+trap 'log "Error on line ${LINENO}: ${BASH_COMMAND}"' ERR
 
 
 GITHUB_BASE="https://github.com/dhis2/"
 GITHUB_CLONE_BASE="https://${GITHUB_USER}:${GITHUB_PASSWORD}@github.com/dhis2/"
-SYNC_DATE=$(date +"%Y%m%d_%H%M%S")
 TX_API3=https://rest.api.transifex.com
 MERGE_FLAG="jenkins-pr-automerge"
-
-# --- options : set the following to `0` to test without pushing anything to remote systems
-PUSH_TRANSLATION_STRINGS=1
-CREATE_PULL_REQUEST=1
 
 # exception list for repos that should not have their PRs closed automatically
 CLOSE_EXCEPTIONS=("data-visualizer-app" "line-listing-app" "dashboard-app" "maps-app")
@@ -51,6 +61,7 @@ git_setup() {
 auto_varl () {
     local BASE="$1"
     declare -a array
+    log "Listing PRs for base=$BASE"
     readarray -t array < <(hub pr list --base="$BASE" --format='%I|%t|%sH%n')
 
     for PR in "${array[@]}"; do
@@ -76,6 +87,7 @@ EOF
             # explicitly check if the CI status of the PR is successful
             if [[ "${ci_status}" == "success" ]]
             then
+                log "Attempting merge of PR ${pr_id}"
                 res=$(hub api --method PUT "repos/{owner}/{repo}/pulls/${pr_id}/merge" --input "$body")
                 echo "Merge PR. Result: $res"
             fi
@@ -95,6 +107,7 @@ EOF
               # if the CI status is a failure, close the PR (A new one will be opened with changes during the next sync)
               if [[ "${ci_status}" == "failure" ]]
               then
+                  log "Closing failed PR ${pr_id}"
                   res=$(gh pr close ${pr_id})
                   echo "Close failed PR. Result: $res"
               fi
@@ -106,14 +119,17 @@ EOF
 }
 
 # --- starting point
-projects=$(curl -s -X GET "$TX_API3/projects?filter[organization]=o:hisp-uio" -H "Content: application/json" -H "Authorization: Bearer $TXTOKEN" | jq '.data[].attributes.slug')
+log "Fetching project list from Transifex"
+projects=$(curl -fSs -X GET "$TX_API3/projects?filter[organization]=o:hisp-uio" -H "Content: application/json" -H "Authorization: Bearer $TXTOKEN" | jq '.data[].attributes.slug')
+log "Projects returned: $(echo "$projects" | wc -l)"
 mkdir temp$$
 pushd temp$$
 
 for p in $projects; do
   # Get the name and the git url of the project
   # The git url is stored in the "homepage" attribute of the transifex project
-  curl -s -X GET "$TX_API3/projects/o:hisp-uio:p:${p//\"/}" -H "Content: application/json" -H "Authorization: Bearer $TXTOKEN" | jq '.[].attributes' >/tmp/proj$$
+  log "Fetching project details for ${p//\"/}"
+  curl -fSs -X GET "$TX_API3/projects/o:hisp-uio:p:${p//\"/}" -H "Content: application/json" -H "Authorization: Bearer $TXTOKEN" | jq '.[].attributes' >/tmp/proj$$
   name=$(cat /tmp/proj$$ | jq '.name')
   tags=$(cat /tmp/proj$$ | jq '.tags | join(",")')
   giturl=$(cat /tmp/proj$$ | jq '.homepage_url')
@@ -125,16 +141,17 @@ for p in $projects; do
   # Loop through all APP projects
   if [[ $tags == *"$MERGE_FLAG"* ]]; then
 
-    echo "Attempting to merge translation PRs for $name : $giturl : $gitslug"
+    log "Attempting to merge translation PRs for $name : $giturl : $gitslug"
 
     # Get the list of branches translated in this project
     # The branch names are at the beginnig of each resource slug, followed by double hyphen '--'
     # The `2.xx` branches appear as `2-xx` and must be converted back (replace hyphen with period)
     # We only want each branch to be listed once
-    branches=$(curl -s -X GET "$TX_API3/resources?filter[project]=o:hisp-uio:p:${p//\"/}" -H "Content: application/json" -H "Authorization: Bearer $TXTOKEN" | jq '.data[].attributes.slug | split("--")[0] | split("-") | join(".")' | uniq)
-    # echo "Branches: $branches"
+    branches=$(curl -fSs -X GET "$TX_API3/resources?filter[project]=o:hisp-uio:p:${p//\"/}" -H "Content: application/json" -H "Authorization: Bearer $TXTOKEN" | jq '.data[].attributes.slug | split("--")[0] | split("-") | join(".")' | uniq)
+    log "Branches discovered: $(echo "$branches" | tr '\n' ' ')"
 
     # clone the project repository and go into it
+    log "Cloning ${GITHUB_CLONE_BASE}${gitslug}"
     git clone --depth 1 --no-single-branch ${GITHUB_CLONE_BASE}${gitslug}
     pushd "$gitslug"
     git_setup
@@ -143,13 +160,13 @@ for p in $projects; do
     for b in $branches; do
       branch=${b//\"/}
 
-      echo "Checking ${branch} branch for translation PRs..."
+      log "Checking ${branch} branch for translation PRs..."
       auto_varl $branch
 
     done
     
     # delete all transifex branches except for unmerged PRs
-    echo "Clean up merged transifex-ALL branches..."
+    log "Clean up merged transifex-ALL branches..."
     hub pr list --format='%H PR#%I%n' > OPEN_PRS
     for g in $(git branch -r | grep 'transifex-ALL' | sed 's/origin\///')
     do 
@@ -177,11 +194,12 @@ for p in $projects; do
             echo "  Skipping open PR: $g"
         fi
       else
+        log "Deleting remote branch $g"
         git push -d origin $g
       fi
     done
     rm -f OPEN_PRS
-    echo "Cleaned."
+    log "Cleaned."
 
     # leave the repo folder
     popd
